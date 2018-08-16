5aa639bcb6140ed0e7f4fd9f51e249a8a59442cd
Jacob Finn <jacobfinn@macbook-pro-9.local>
2018-08-16 10:21:48 -0500

    Initial Commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Jacob Finn on 8/16/18.
+//  Copyright © 2018 Jacob Finn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
